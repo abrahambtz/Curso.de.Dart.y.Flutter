529f75d9f66c016b6eb1dd35b3023f5e75a5ae74
abrahambtz <43942273+abrahambtz@users.noreply.github.com>
2020-10-20 13:13:38 -0500

    contador_page.dart Informacion
    El archivo contador_page.dart este archivo devuelve un widget
    StatefulWidget lo que nos permite generar y cambiar su estados, este
    tiene un Scaffold donde muestra toda la pantalla.
    
    "Este archivo realiza todas las operaciones de la aplicacion contador."

---
+++ Seccion 5/contador/lib/src/pages/contador_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  @override
+  createState() => _ContadorPageState();
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  //_ContadorPageState Fuerzas a que la clase se privada
+  final _estiloTexto = new TextStyle(fontSize: 25);
+  final _estiloColor = new Color(0xFFFA0000);
+
+  int _contador = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Contador, App 1 - V2'),
+          centerTitle: true,
+          elevation: 10.00,
+          backgroundColor: Color(0xFFFB0000),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Numero de cick!',
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                '$_contador',
+                style: _estiloTexto,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBottones()
+        //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        );
+  }
+
+  Widget _crearBottones() {
+    return Row(
+      children: [
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _reset,
+          backgroundColor: _estiloColor,
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _sustraer,
+          backgroundColor: _estiloColor,
+        ),
+        SizedBox(width: 10.0),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _add,
+          backgroundColor: _estiloColor,
+        ),
+      ],
+    );
+  }
+
+  void _add() => setState(() => _contador++);
+  void _sustraer() => setState(() => _contador--);
+  void _reset() => setState(() => _contador = 0);
+}

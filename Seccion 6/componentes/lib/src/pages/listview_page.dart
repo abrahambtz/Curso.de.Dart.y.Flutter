5ab7481c78d99400c2b24d0aba60f0ad21621cad
abrahambtz <43942273+abrahambtz@users.noreply.github.com>
2020-11-11 14:38:35 -0600

    Capitulo 86. Agregando un ListView Builder.
    En este capitulo se agrego una lista dinamica, esta cambia de acuerdo a
    la imagenes que se indico en arreglo/lista.
    
    Page: listview_page.dart.

---
+++ Seccion 6/componentes/lib/src/pages/listview_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  ListPage({Key key}) : super(key: key);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  List<int> _listaNumeros = [10, 20, 30, 40, 50];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        final imagen = _listaNumeros[index];
+        // print("$index");
+        return FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          image: NetworkImage("https://picsum.photos/500/300/?image=$imagen"),
+        );
+      },
+      itemCount: _listaNumeros.length,
+    );
+  }
+}
